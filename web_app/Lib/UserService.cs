48323ef363211aa431501cc1c5ad25fb7d5c8ec9
NoNeedToArgue <genius_x@mail.ru>
2023-05-19 11:41:41 +0300

    Add Redis

---
+++ web_app/Lib/UserService.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.Caching.Distributed;
+using System.Text.Json;
+
+namespace Lib
+{
+    public class UserService
+    {
+        IDistributedCache cache;
+       
+        public UserService(IDistributedCache distributedCache)
+        {
+            cache = distributedCache;
+        }
+
+        public async Task<User?> GetUser(int id)
+        {
+            User? user = null;
+           
+            var userString = await cache.GetStringAsync(id.ToString());
+            if (userString != null) user = JsonSerializer.Deserialize<User>(userString);
+
+            return user;
+        }
+
+        public async void SetUser(User user)
+        {
+            var userString = JsonSerializer.Serialize(user);
+            
+            await cache.SetStringAsync(user.Id.ToString(), userString, new DistributedCacheEntryOptions
+            {
+                AbsoluteExpirationRelativeToNow = TimeSpan.FromMinutes(5)
+            });
+        }
+    }
+}
--- web_app/Program.cs
+++ web_app/Program.cs
@@ -3,7 +3,46 @@ using System.Text;
 using Prometheus;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddTransient<UserService>();
+
+builder.Services.AddStackExchangeRedisCache(options => {
+    options.Configuration = "redis:6379";
+    options.InstanceName = "test";
+});
+
 var app = builder.Build();
+
+app.MapGet("/user/{id}", async (int id, UserService userService) =>
+{
+    app.Logger.LogInformation($"Path: /user/{id}  Time: {DateTime.Now.ToLongTimeString()}");
+
+    string response = "User not found\nTwo second database access latency was simulated";
+
+    User? user = await userService.GetUser(id);
+    if (user != null)
+    {
+        response = $"User received from cache\n\nUser {user.Name}  Id = {user.Id}  City = {user.City}";
+    }
+    else
+    {
+        Thread.Sleep(2000);
+
+        using (ApplicationContext db = new())
+        {
+            user = db.Users.Where(u => u.Id == id).SingleOrDefault();
+        }
+        if (user != null)
+        {
+            userService.SetUser(user);
+            response = $"User received from db\nTwo second latency was simulated\n\nUser {user.Name}  Id = {user.Id}  City = {user.City}";
+        }
+    }
+    
+    app.Logger.LogInformation(response);
+
+    return response;
+});
 
 app.MapGet("/getusers", async (context) =>
 {
@@ -38,53 +77,74 @@ app.MapGet("/getusers", async (context) =>
         }
     }
 
-    await context.Response.WriteAsync(stringBuilder.ToString());
+    string response = stringBuilder.ToString().Length > 0 ? stringBuilder.ToString().Trim() : "No records";
+
+    app.Logger.LogInformation(response);
+
+    await context.Response.WriteAsync(response);
 });
 
-app.MapGet("/producer", (ILogger<Program> logger) =>
+app.MapGet("/producer", () =>
 {
+    app.Logger.LogInformation($"Path: /producer  Time: {DateTime.Now.ToLongTimeString()}");
+    
     string messageInfo = Producer.Send();
-    app.Logger.LogInformation($"Path: /producer  Time: {DateTime.Now.ToLongTimeString()}\n{messageInfo}");
+    
+    app.Logger.LogInformation(messageInfo);
+
     return messageInfo;
 });
 
-app.MapGet("/consumer", (ILogger<Program> logger) =>
+app.MapGet("/consumer", () =>
 {
+    app.Logger.LogInformation($"Path: /consumer  Time: {DateTime.Now.ToLongTimeString()}");
+
     string messageInfo = Consumer.Receive();
-    app.Logger.LogInformation($"Path: /consumer  Time: {DateTime.Now.ToLongTimeString()}\n{messageInfo}");
+
+    app.Logger.LogInformation(messageInfo);
+
     return messageInfo;
 });
 
 app.MapGet("/", async (context) =>
 {
-    app.Logger.LogInformation($"Processing request {context.Request.QueryString}  Time: {DateTime.Now.ToLongTimeString()}");
-    
+    app.Logger.LogInformation($"Path: /  Time: {DateTime.Now.ToLongTimeString()}");
+    if (context.Request.QueryString.ToString().Count() > 0)
+        app.Logger.LogInformation($"Processing request {context.Request.QueryString}  Time: {DateTime.Now.ToLongTimeString()}");
+
     context.Response.ContentType = "text/html; charset=utf-8";
+
     var stringBuilder = new StringBuilder("<h3>Request</h3><table>");
     stringBuilder.Append("<tr><td>Parameter</td><td>Value</td></tr>");
-    string? username = null;
+    
+    string? name = null;
     string? city = null;
+    
     foreach (var param in context.Request.Query)
     {
         if (param.Key.ToLower() == "name")
         {
-            username = param.Value;
+            name = param.Value;
         }
         if (param.Key.ToLower() == "city")
         {
             city = param.Value;
         }
+
         stringBuilder.Append($"<tr><td>{param.Key}</td><td>{param.Value}</td></tr>");
     }
-    if (username != null && city != null)
+
+    if (name != null && city != null)
     {
         using (ApplicationContext db = new ApplicationContext())
         {
-            db.Users.Add(new User { Name = username, City = city });
+            db.Users.Add(new User { Name = name, City = city });
             db.SaveChanges();
         } 
     }
+
     stringBuilder.Append("</table>");
+    
     await context.Response.WriteAsync(stringBuilder.ToString());
 });
 
